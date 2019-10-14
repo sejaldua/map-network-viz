bb0bd2daa98adf8463deca0e8366a905162508e8
Sejal <sejal.dua@tufts.edu>
2019-10-14 02:15:48 +0200

    3 more cities and better instructions in README

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ What you'll need to do (via Terminal) in order to use this program:
 * `conda install -c anaconda networkx`
 * `conda install geopandas`
 * `conda install -c anaconda pillow`
+* `pip install geopy`
 
 When you are prompted to input a city, you must enter it in a format
 that matches the following:
--- mapmaker.py
+++ mapmaker.py
@@ -1,3 +1,4 @@
+#imports
 import networkx as nx
 import osmnx as ox
 import requests
@@ -9,6 +10,8 @@ from geopy.geocoders import Nominatim
 from PIL import Image, ImageOps, ImageColor, ImageFont, ImageDraw
 geolocator = Nominatim()
 
+#--------------------------------------------------------------------
+
 # define city/cities
 while True:
         try:
@@ -19,12 +22,18 @@ while True:
                 print("Sorry, that format was not recognized by OpenStreetMap. Try again.")
 
 
-# Center of map
+# center of map
 loc = geolocator.geocode(city)
 latitude = loc.latitude
 longitude = loc.longitude
+print('------------------------')
+print('latitude:', latitude)
+print('longitude:', longitude)
+print('------------------------')
 
 print("data: obtained")
+
+#--------------------------------------------------------------------
 
 # unpack data
 u, v, key, data = [], [], [], []
@@ -36,27 +45,25 @@ for u_elem, v_elem, key_elem, data_elem in G.edges(keys = True, data = True):
 
 print("data: unpacked")
 
+#--------------------------------------------------------------------
+
 # assign each segment a color based on its length
 roadColors = []
 for item in data:
     if "length" in item.keys():
-        
         if item["length"] <= 100:
             color = "#d40a47"
-            
         elif item["length"] > 100 and item["length"] <= 200:
             color = "#e78119"
-            
         elif item["length"] > 200 and item["length"] <= 400:
             color = "#30bab0"
-            
         elif item["length"] > 400 and item["length"] <= 800:
             color = "#bbbbbb"
-            
         else:
             color = "w"
-             
     roadColors.append(color)
+
+#--------------------------------------------------------------------
 
 # assign each segment a width based on its type
 roadWidths = []
@@ -68,11 +75,15 @@ for item in data:
 
         roadWidths.append(linewidth)
 
-# Bbox sides
+#--------------------------------------------------------------------
+
+# bbox sides
 north = latitude + 0.035
 south = latitude - 0.035
 east = longitude + 0.05
 west = longitude - 0.05
+
+#--------------------------------------------------------------------
 
 print("drawing map")
 
@@ -81,26 +92,32 @@ fig, ax = ox.plot_graph(G, node_size=0, bbox = (north, south, east, west), margi
 
 print("plotted graph")
 
-# Text and marker size
+#--------------------------------------------------------------------
+
+# text and marker size
 markersize = 12
 fontsize = 12
 
-# Add legend
+# add legend
 legend_elements = [Line2D([0], [0], marker='s', color="#061529", label= 'Length < 100 m', markerfacecolor="#d40a47", markersize=markersize), Line2D([0], [0], marker='s', color="#061529", label= 'Length between 100-200 m', markerfacecolor="#e78119", markersize=markersize), Line2D([0], [0], marker='s', color="#061529", label= 'Length between 200-400 m', markerfacecolor="#30bab0", markersize=markersize), Line2D([0], [0], marker='s', color="#061529", label= 'Length between 400-800 m', markerfacecolor="#bbbbbb", markersize=markersize), Line2D([0], [0], marker='s', color="#061529", label= 'Length > 800 m', markerfacecolor="w", markersize=markersize)]    
                       
 l = ax.legend(handles=legend_elements, bbox_to_anchor=(0.0, 0.0), frameon=True, ncol=1, facecolor = '#061529', framealpha = 0.9, loc='lower left',  fontsize = fontsize, prop={'family':"Georgia", 'size':fontsize})  
 
-print("made the legend")
-  
-# Legend font color
+# legend font color
 for text in l.get_texts():
     text.set_color("w")
+
+print("made the legend")
+
+#--------------------------------------------------------------------
     
 print("saving")
 
-# Save figure
+# save figure
 name = city[0:(city.find(','))]
 name = name.replace(" ", "_")
 fig.savefig(name + ".png", dpi=300, bbox_inches='tight', format="png", facecolor=fig.get_facecolor(), transparent=True)
+
+#--------------------------------------------------------------------
 
 
